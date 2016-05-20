2909b61cd5867e300c197e987c43ebc0c91eb247
Jer Emiah Moore <moore.jeremiah.mark@gmail.com>
2016-05-20 10:12:10 -0600

    if this overwrites my finished change I'm going t
    kill someone

--- 0007.py
+++ 0007.py
@@ -2,18 +2,23 @@ from PIL import Image
 import os
 l=[]
 i=0
+l.append(i)
 im = Image.open('0007.png')
 for y in range(0,95):
 	for x in range(0,629):
 		pix=im.getpixel((x,y))
-		if pix[0] == pix[1] and pix[0] == pix[2]:
-			l.append(chr(pix[0]))
+		if pix[0] == pix[1] and pix[0] == pix[2] and pix[0] != l[-1] and pix[0] >= 31 and pix[0] <= 127:
+			l.append(pix[0])
 s=''
-j=0
-while j < len(l):
-	str1 = s.join(str(l[j]))
-	j+=1
-print str1
+s = ''.join(chr(a) for a in l)
+print (s)
+# should output smartguy,youmadeit.thenextlevelis[105,10,16,101,103,14,105,16,121]
+r = []
+q = [105,10,16,101,103,14,105,16,121]
+for t in q:
+	r.append(chr(t))
+	print t
+print r
+#i
+#dleegsoidley
 
-str1= ''.join(l)
-print str1
